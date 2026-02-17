ae8012e4e7baf530978763efda9baa1398e4a9b0
Daniel Russo-Batterham <daniel.russo@unimelb.edu.au>
2026-02-18 09:21:24 +1100

    Update base url configuration

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.GITHUB_ACTIONS ? '/spotify-unpacked/' : '/',
   plugins: [
     vue(),
     vueDevTools(),
