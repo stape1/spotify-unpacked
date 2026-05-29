b5aeb7de72bf34558bb8630f7e32a6904bccb08a
Maz <mazgeekery@gmail.com>
2026-05-29 11:36:52 +1000

    Broke then fixed the vizstore import

--- src/visualisations/UploadStatusViz.vue
+++ src/visualisations/UploadStatusViz.vue
@@ -1,21 +1,4 @@
 <script setup lang="ts">
-
-import { computed } from 'vue'
-import { useDark } from '@vueuse/core'
-import { useDataStore } from '@/stores/data'
-
-
-const dataStore = useDataStore()
-const isDark = useDark({ storageKey: 'spotify-unpacked-colour-mode' })
-const currentData = computed(() => dataStore.getChartData(visStore.selectedChart) as any)
-
-const baseOptions = computed(() => {
-  const textColour = isDark.value ? 'rgba(255,255,255,0.85)' : 'rgba(0,0,0,0.8)'
-  const gridColour = isDark.value ? 'rgba(255,255,255,0.1)' : 'rgba(0,0,0,0.1)'
-  return { textColour, gridColour }
-})
-
-
 </script>
 <template>
   <div class="mx-auto max-w-4xl p-6 space-y-6">
